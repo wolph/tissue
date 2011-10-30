de3717f29703f21e342b438b00b04f103fff50c4
Jason Kölker <jason@koelker.net>
2011-10-30 02:09:31 -0500

    reorg

--- setup.py
+++ setup.py
@@ -1,26 +1,26 @@
 from setuptools import setup, find_packages
-import sys, os
 
-version = '0.1'
+version = "0.1"
 
-setup(name='tissue',
+setup(name="tissue",
       version=version,
       description="Tissue - automated pep8 checker for nose",
       long_description="""\
 """,
       classifiers=[], # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
-      keywords='',
-      author='Jason K\xc3\xb6lker',
-      author_email='jason@koelker.net',
-      url='https://github.com/jkoelker/tissue',
-      license='GNU LGPL',
-      packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
-      include_package_data=True,
-      zip_safe=True,
+      keywords="",
+      author="Jason K\xc3\xb6lker",
+      author_email="jason@koelker.net",
+      url="https://github.com/jkoelker/tissue",
+      license="GNU LGPL",
+      py_modules=["tissue"],
       install_requires=[
-          # -*- Extra requirements: -*-
+          "nose",
+          "pep8",
       ],
       entry_points="""
       # -*- Entry points: -*-
+[nose.plugins.0.10]
+tissue = tissue:Tissue
       """,
       )
