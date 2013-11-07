2892abc845d82404760576e51ce27012ae918a0f
WoLpH <Rick@Fawo.nl>
2013-11-07 06:39:26 +0100

    pep8ified and cleaned up useless code

--- tissue.py
+++ tissue.py
@@ -1,8 +1,7 @@
-"""
+'''
 PEP8 automated checker for nose. Based on coverage plugin
-"""
+'''
 import logging
-import os
 import sys
 
 import pep8
@@ -11,28 +10,33 @@ from nose import plugins
 from nose import util
 
 
-log = logging.getLogger("nose.plugins.tissue")
+log = logging.getLogger('nose.plugins.tissue')
 
 
 # thanks pinocchio nose plugin for this code
-color_end = "\x1b[1;0m"
-colors = dict(green="\x1b[1;32m", red="\x1b[1;31m", yellow="\x1b[1;33m")
+color_end = '\x1b[1;0m'
+colors = dict(green='\x1b[1;32m', red='\x1b[1;31m', yellow='\x1b[1;33m')
 
 
 def in_color(color, text):
-    """
+    '''
     Colorize text, adding color to each line so that the color shows up
     correctly with the less -R as well as more and normal shell.
-    """
-    return "".join("%s%s%s" % (colors[color], line, color_end)
-                            for line in text.splitlines(True))
+    '''
+    return ''.join('%s%s%s' % (colors[color], line, color_end)
+                   for line in text.splitlines(True))
 
 
 class TissueReport(pep8.StandardReport):
-    '''pep8's Standard report with a get_file_results that accepts an optional stream arg to output to'''
+    '''
+    pep8's Standard report with a get_file_results that accepts an optional
+    stream arg to output to.
+    '''
 
     def get_file_results(self, stream=sys.stdout):
-        """Write the result to stream and return the overall count for this file."""
+        '''
+        Write the result to stream and return the overall count for this file.
+        '''
         self._deferred_print.sort()
         for line_number, offset, code, text, doc in self._deferred_print:
             stream.write(self._fmt % {
@@ -53,9 +57,10 @@ class TissueReport(pep8.StandardReport):
 
 
 class Tissue(plugins.Plugin):
-    """Automated PEP8 checked for nose"""
 
-    name = "tissue"
+    '''Automated PEP8 checked for nose'''
+
+    name = 'tissue'
 
     def begin(self):
         self.messages = []
@@ -86,7 +91,7 @@ class Tissue(plugins.Plugin):
         self.pep8.input_dir(path)
 
     def beforeImport(self, filename, module):
-        if filename.endswith(".py") and self.want_file(filename):
+        if filename.endswith('.py') and self.want_file(filename):
             self.pep8.input_file(filename)
 
     def configure(self, options, config):
@@ -99,28 +104,26 @@ class Tissue(plugins.Plugin):
                 self.tissue_packages.extend(pkgs)
         self.tissue_inclusive = options.tissue_inclusive
         if self.tissue_packages:
-            log.info("PEP8 report will include only packages: %s",
+            log.info('PEP8 report will include only packages: %s',
                      self.tissue_packages)
 
-        # NOTE(jkoelker) Urgh! Really? Global options? At least there is a
-        #                function that takes the arglist ;(
         arglist = []
         if options.tissue_repeat:
-            arglist.append("--repeat")
+            arglist.append('--repeat')
 
         if options.tissue_select:
-            arglist.append("--select")
+            arglist.append('--select')
             arglist.append(options.tissue_select)
 
         if options.tissue_ignore:
-            arglist.append("--ignore")
+            arglist.append('--ignore')
             arglist.append(options.tissue_ignore)
 
         if options.tissue_show_source:
-            arglist.append("--show-source")
+            arglist.append('--show-source')
 
         if options.tissue_show_pep8:
-            arglist.append("--show-pep8")
+            arglist.append('--show-pep8')
 
         options, paths = pep8.process_options(arglist)
         self.pep8 = pep8.StyleGuide(**options.__dict__)
@@ -128,60 +131,53 @@ class Tissue(plugins.Plugin):
 
     def options(self, parser, env):
         plugins.Plugin.options(self, parser, env)
-        parser.add_option("--tissue-package", action="append",
-                          default=env.get("NOSE_TISSUE_PACKAGE"),
-                          metavar="PACKAGE",
-                          dest="tissue_packages",
-                          help="Restrict pep8 output to selected packages "
-                               "[NOSE_TISSUE_PACKAGE]")
-        parser.add_option("--tissue-inclusive", action="store_true",
-                          dest="tissue_inclusive",
-                          default=env.get("NOSE_TISSUE_INCLUSIVE"),
-                          help="Include all python files under working "
-                               "directory in pep8 run. "
-                               "[NOSE_TISSUE_INCLUSIVE]")
-        parser.add_option("--tissue-repeat", action="store_true",
-                          default=env.get("NOSE_TISSUE_REPEAT"),
-                          metavar="ERRORS",
-                          dest="tissue_repeat",
-                          help="Show all occurrences of the same error "
-                               "[NOSE_TISSUE_REPEAT]")
-        parser.add_option("--tissue-select",
-                          default=env.get("NOSE_TISSUE_SELECT"),
-                          metavar="ERRORS",
-                          dest="tissue_select",
-                          help="Select errors and warnings (e.g. E,W6) "
-                               "[NOSE_TISSUE_SELECT]")
-        parser.add_option("--tissue-ignore",
-                          default=env.get("NOSE_TISSUE_ignore"),
-                          metavar="ERRORS",
-                          dest="tissue_ignore",
-                          help="Skip errors and warnings (e.g. E,W6) "
-                               "[NOSE_TISSUE_IGNORE]")
-        parser.add_option("--tissue-show-source", action="store_true",
-                          dest="tissue_show_source",
-                          default=env.get("NOSE_TISSUE_SHOW_SOURCE"),
-                          help="Show source code for each error "
-                               "[NOSE_TISSUE_SHOW_SOURCE]")
-        parser.add_option("--tissue-show-pep8", action="store_true",
-                          dest="tissue_show_pep8",
-                          default=env.get("NOSE_TISSUE_SHOW_TISSUE"),
-                          help="Show text of PEP 8 for each error "
-                               "[NOSE_TISSUE_SHOW_TISSUE]")
-        parser.add_option("--tissue-statistics", action="store_true",
-                          dest="tissue_statistics",
-                          default=env.get("NOSE_TISSUE_STATISTICS"),
-                          help="Count errors and warnings "
-                               "[NOSE_TISSUE_STATISTICS]")
-        parser.add_option("--tissue-color", action="store_true",
-                          dest="tissue_color",
-                          default=env.get("NOSE_TISSUE_COLOR"),
-                          help="Show errors and warnings using colors "
-                               "[NOSE_TISSUE_COLOR]")
+        parser.add_option('--tissue-package', action='append',
+                          default=env.get('NOSE_TISSUE_PACKAGE'),
+                          metavar='PACKAGE',
+                          dest='tissue_packages',
+                          help='Restrict pep8 output to selected packages '
+                               '[NOSE_TISSUE_PACKAGE]')
+        parser.add_option('--tissue-inclusive', action='store_true',
+                          default=env.get('NOSE_TISSUE_INCLUSIVE'),
+                          help='Include all python files under working '
+                               'directory in pep8 run. '
+                               '[NOSE_TISSUE_INCLUSIVE]')
+        parser.add_option('--tissue-repeat', action='store_true',
+                          default=env.get('NOSE_TISSUE_REPEAT'),
+                          metavar='ERRORS',
+                          dest='tissue_repeat',
+                          help='Show all occurrences of the same error '
+                               '[NOSE_TISSUE_REPEAT]')
+        parser.add_option('--tissue-select',
+                          default=env.get('NOSE_TISSUE_SELECT'),
+                          metavar='ERRORS',
+                          help='Select errors and warnings (e.g. E,W6) '
+                               '[NOSE_TISSUE_SELECT]')
+        parser.add_option('--tissue-ignore',
+                          default=env.get('NOSE_TISSUE_ignore'),
+                          metavar='ERRORS',
+                          help='Skip errors and warnings (e.g. E,W6) '
+                               '[NOSE_TISSUE_IGNORE]')
+        parser.add_option('--tissue-show-source', action='store_true',
+                          default=env.get('NOSE_TISSUE_SHOW_SOURCE'),
+                          help='Show source code for each error '
+                               '[NOSE_TISSUE_SHOW_SOURCE]')
+        parser.add_option('--tissue-show-pep8', action='store_true',
+                          default=env.get('NOSE_TISSUE_SHOW_TISSUE'),
+                          help='Show text of PEP 8 for each error '
+                               '[NOSE_TISSUE_SHOW_TISSUE]')
+        parser.add_option('--tissue-statistics', action='store_true',
+                          default=env.get('NOSE_TISSUE_STATISTICS'),
+                          help='Count errors and warnings '
+                               '[NOSE_TISSUE_STATISTICS]')
+        parser.add_option('--tissue-color', action='store_true',
+                          default=env.get('NOSE_TISSUE_COLOR'),
+                          help='Show errors and warnings using colors '
+                               '[NOSE_TISSUE_COLOR]')
 
     def report(self, stream):
         report = self.pep8.check_files()
-        stream.write('\n' + "PEP8:" + '\n')
+        stream.write('\n' + 'PEP8:' + '\n')
         report.get_file_results(stream)
         if self.tissue_statistics:
             stats = '\n'.join(report.get_statistics())
@@ -189,7 +185,7 @@ class Tissue(plugins.Plugin):
 
     def wantFile(self, file, package=None):
         if self.tissue_inclusive:
-            if file.endswith(".py"):
+            if file.endswith('.py'):
                 if package and self.tissue_packages:
                     for want in self.tissue_packages:
                         if package.startswith(want):
