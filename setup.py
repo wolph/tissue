0d7f9d8ab35534bb913896822b24be0eab7e426a
WoLpH <Rick@Fawo.nl>
2013-11-07 06:26:39 +0100

    updated setup to newer style entry points and pep8ified

--- setup.py
+++ setup.py
@@ -1,25 +1,26 @@
 from setuptools import setup
 
-version = "0.8"
+version = '0.9'
 
-setup(name="tissue",
-      version=version,
-      description="Tissue - automated pep8 checker for nose",
-      long_description=open("README.rst").read(),
-      classifiers=[],  # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
-      keywords="pep8 nose",
-      author="Jason K\xc3\xb6lker",
-      author_email="jason@koelker.net",
-      url="https://github.com/jkoelker/tissue",
-      license="GNU LGPL",
-      py_modules=["tissue"],
-      install_requires=[
-          "nose",
-          "pep8",
-      ],
-      entry_points="""
-      # -*- Entry points: -*-
-[nose.plugins.0.10]
-tissue = tissue:Tissue
-      """,
-      )
+setup(
+    name='tissue',
+    version=version,
+    description='Tissue - automated pep8 checker for nose',
+    long_description=open('README.rst').read(),
+    classifiers=[],
+    keywords='pep8 nose',
+    author='Jason K\xc3\xb6lker',
+    author_email='jason@koelker.net',
+    url='https://github.com/jkoelker/tissue',
+    license='GNU LGPL',
+    py_modules=['tissue'],
+    install_requires=[
+        'nose',
+        'pep8',
+    ],
+    entry_points={
+        'nose.plugins': [
+            'tissue = tissue:Tissue',
+        ],
+    },
+)
